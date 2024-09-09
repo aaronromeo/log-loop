a69fd06a85a2a0563ecdea39def412b766b7e63d
Aaron Romeo <aaron.romeo@salesforce.com>
2024-09-09 11:34:12 -0400

    bacon

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	go func() {
+		fmt.Println(time.Now())
+		time.Sleep(time.Second * 60)
+	}()
+
+	router.Run(":" + port)
+}
